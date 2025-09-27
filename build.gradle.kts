2632cc26ad7b083e960db2f09e1277ad4bcfd0b0
yugo-miyabe <yuni42yuyuyu@gmail.com>
2025-09-27 21:59:24 +0900

    依存関係を追加

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,4 +7,6 @@ plugins {
     alias(libs.plugins.composeCompiler) apply false
     alias(libs.plugins.kotlinMultiplatform) apply false
     alias(libs.plugins.kotlin.serialization) apply false
+    alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.androidx.room) apply false
 }
