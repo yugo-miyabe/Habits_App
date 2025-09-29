fd752682f127a5888a611c01fe3a48deb12c72e0
yugo-miyabe <yuni42yuyuyu@gmail.com>
2025-09-30 00:19:53 +0900

    インテントの整理

--- composeApp/src/commonMain/kotlin/jp/yuyuyu/habits/ui/organisms/AppBar.kt
+++ composeApp/src/commonMain/kotlin/jp/yuyuyu/habits/ui/organisms/AppBar.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.RowScope
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
@@ -27,7 +28,7 @@ fun TopBar(
                 Text(
                     text = title,
                     textAlign = TextAlign.Center,
-                    // style = Typography.labelLarge
+                    style = MaterialTheme.typography.labelLarge
                 )
             }
         },
--- composeApp/src/commonMain/kotlin/jp/yuyuyu/habits/ui/template/HomeTemplate.kt
+++ composeApp/src/commonMain/kotlin/jp/yuyuyu/habits/ui/template/HomeTemplate.kt
@@ -4,12 +4,12 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import habits.composeapp.generated.resources.Res
-import habits.composeapp.generated.resources.compose_multiplatform
 import habits.composeapp.generated.resources.settings_24dp
 import jp.yuyuyu.habits.ui.organisms.TopBar
 import org.jetbrains.compose.resources.painterResource
@@ -19,25 +19,22 @@ import org.jetbrains.compose.ui.tooling.preview.Preview
 fun HomeTemplate(
     onSettingClick: () -> Unit,
 ) {
-    Scaffold(
-        topBar = {
-            TopBar(
-                actions = {
-                    IconButton(onClick = onSettingClick) {
-                        Icon(
-                            painter = painterResource(Res.drawable.settings_24dp),
-                            contentDescription = null,
-                        )
-                    }
-                }
-            )
-        }
-    ) { innerPadding ->
+    Scaffold(topBar = {
+        TopBar(actions = {
+            IconButton(onClick = onSettingClick) {
+                Icon(
+                    painter = painterResource(Res.drawable.settings_24dp),
+                    contentDescription = null,
+                )
+            }
+        })
+    }) { innerPadding ->
         Text(
             modifier = Modifier
                 .padding(innerPadding)
                 .fillMaxHeight(),
-            text = "Home"
+            text = "Home",
+            style = MaterialTheme.typography.bodyLarge
         )
     }
 }
