68f0268ba2e5c7b097f5438f1690118536c603ea
yugo-miyabe <yuni42yuyuyu@gmail.com>
2025-11-02 18:31:27 +0900

    ライセンス画面追加

--- composeApp/src/commonMain/kotlin/jp/yuyuyu/habits/ui/template/SettingTemplate.kt
+++ composeApp/src/commonMain/kotlin/jp/yuyuyu/habits/ui/template/SettingTemplate.kt
@@ -1,20 +1,27 @@
 package jp.yuyuyu.habits.ui.template
 
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
+import habits.composeapp.generated.resources.Res
+import habits.composeapp.generated.resources.licence
+import jp.yuyuyu.habits.ui.organisms.ListItemCell
+import org.jetbrains.compose.resources.stringResource
+import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 fun SettingTemplate() {
     Scaffold { innerPadding ->
-        Text(
-            modifier = Modifier
-                .padding(innerPadding)
-                .fillMaxHeight(),
-            text = "SettingTemplate"
-        )
+        Column {
+            ListItemCell(
+                title = stringResource(Res.string.licence)
+            )
+        }
     }
+}
+
+@Composable
+@Preview(showBackground = true)
+private fun SettingTemplate_Preview() {
+    SettingTemplate()
 }
