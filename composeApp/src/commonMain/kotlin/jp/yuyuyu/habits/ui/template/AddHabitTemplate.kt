f9a8d7873653bc6e587ba09d80a7e8fc3a0ec8c6
yugo-miyabe <yuni42yuyuyu@gmail.com>
2025-11-03 22:38:31 +0900

    戻るボタンの追加

--- composeApp/src/commonMain/kotlin/jp/yuyuyu/habits/ui/template/AddHabitTemplate.kt
+++ composeApp/src/commonMain/kotlin/jp/yuyuyu/habits/ui/template/AddHabitTemplate.kt
@@ -1,21 +1,33 @@
 package jp.yuyuyu.habits.ui.template
 
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import habits.composeapp.generated.resources.Res
+import habits.composeapp.generated.resources.add_habits
+import jp.yuyuyu.habits.ui.organisms.TopBar
+import org.jetbrains.compose.resources.stringResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 fun AddHabitTemplate(
     onBackClick: () -> Unit
 ) {
-
-    Scaffold { innerPadding ->
-        Column(modifier = Modifier.padding(innerPadding)) {
-            Text("Add Habit Screen")
+    Scaffold(
+        topBar = {
+            TopBar(
+                title = stringResource(Res.string.add_habits),
+                onBackClick = onBackClick,
+            )
+        }
+    ) { innerPadding ->
+        LazyColumn(modifier = Modifier.padding(innerPadding)) {
+            item {
+                Text("テキスト")
+            }
         }
     }
 }
