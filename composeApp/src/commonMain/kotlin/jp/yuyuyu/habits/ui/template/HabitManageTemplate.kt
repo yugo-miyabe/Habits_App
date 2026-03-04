106d0841ed6f7de876d72e1d75a045c9674cd3d9
yugo-miyabe <yuni42yuyuyu@gmail.com>
2026-03-05 00:18:38 +0900

    削除ボタンを追加

--- composeApp/src/commonMain/kotlin/jp/yuyuyu/habits/ui/template/HabitManageTemplate.kt
+++ composeApp/src/commonMain/kotlin/jp/yuyuyu/habits/ui/template/HabitManageTemplate.kt
@@ -1,18 +1,26 @@
 package jp.yuyuyu.habits.ui.template
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import habits.composeapp.generated.resources.Res
+import habits.composeapp.generated.resources.delete_24dp
 import jp.yuyuyu.habits.screen.habitManage.HabitManageUiState.HabitManageItem
 import jp.yuyuyu.habits.theme.AppTheme
 import jp.yuyuyu.habits.ui.organisms.TopBar
+import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
@@ -22,7 +30,9 @@ fun HabitManageTemplate(
 ) {
     Scaffold(
         topBar = {
-            TopBar(onBackClick = onBackClick)
+            TopBar(
+                onBackClick = onBackClick,
+            )
         }
     ) { innerPadding ->
         LazyColumn(
@@ -30,17 +40,30 @@ fun HabitManageTemplate(
         ) {
             items(
                 items = habitList, key = { it.habitId }) { habit ->
-                Text(
-                    text = habit.title,
-                    style = AppTheme.typography.titleLarge,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .clickable(
-                            onClick = { /* TODO: 画面遷移実装 */ }
-                        )
-                        .padding(horizontal = 16.dp)
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    modifier = Modifier.fillMaxWidth().padding(horizontal = 16.dp)
                         .padding(vertical = 8.dp)
-                )
+                ) {
+                    Text(
+                        text = habit.title,
+                        style = AppTheme.typography.titleLarge,
+                        modifier = Modifier
+                            .clickable(
+                                onClick = { /* TODO */ }
+                            )
+                            .weight(1f),
+                    )
+                    IconButton(
+                        onClick = { /* TODO */ },
+                    ) {
+                        Icon(
+                            painter = painterResource(Res.drawable.delete_24dp),
+                            contentDescription = null,
+                        )
+                    }
+                }
             }
         }
     }
@@ -63,4 +86,3 @@ private fun HabitManageTemplatePreview() {
         onBackClick = { /* preview */ },
     )
 }
-
