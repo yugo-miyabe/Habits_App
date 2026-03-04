979a43a92903fd7350efed77d25747b3aca69207
yugo-miyabe <yuni42yuyuyu@gmail.com>
2026-03-04 22:57:08 +0900

    リストを追加

--- composeApp/src/commonMain/kotlin/jp/yuyuyu/habits/ui/template/HabitManageTemplate.kt
+++ composeApp/src/commonMain/kotlin/jp/yuyuyu/habits/ui/template/HabitManageTemplate.kt
@@ -1,8 +1,17 @@
 package jp.yuyuyu.habits.ui.template
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import jp.yuyuyu.habits.screen.habitManage.HabitManageUiState.HabitManageItem
+import jp.yuyuyu.habits.theme.AppTheme
 import jp.yuyuyu.habits.ui.organisms.TopBar
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
@@ -15,8 +24,25 @@ fun HabitManageTemplate(
         topBar = {
             TopBar(onBackClick = onBackClick)
         }
-    ) {
-        // TODO: 画面作成
+    ) { innerPadding ->
+        LazyColumn(
+            modifier = Modifier.padding(innerPadding)
+        ) {
+            items(
+                items = habitList, key = { it.habitId }) { habit ->
+                Text(
+                    text = habit.title,
+                    style = AppTheme.typography.titleLarge,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .clickable(
+                            onClick = { /* TODO: 画面遷移実装 */ }
+                        )
+                        .padding(horizontal = 16.dp)
+                        .padding(vertical = 8.dp)
+                )
+            }
+        }
     }
 }
 
@@ -27,10 +53,10 @@ private fun HabitManageTemplatePreview() {
         habitList = listOf(
             HabitManageItem(
                 habitId = 1,
-                title = "\uD83D\uDCAA筋トレ\"",
+                title = "\uD83D\uDCAA筋トレ",
             ),
             HabitManageItem(
-                habitId = 1,
+                habitId = 2,
                 title = "\uD83C\uDF05 早起き",
             ),
         ),
